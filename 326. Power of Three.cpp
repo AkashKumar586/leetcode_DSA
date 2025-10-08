f71d6011c4add21540f2cea2babc0d990cc97d98
AkashKumar586 <princeakash259@gmail.com>
2025-10-08 06:32:00 +0530

    c

---
+++ 326. Power of Three.cpp
@@ -0,0 +1,22 @@
+326. Power of Three
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Given an integer n, return true if it is a power of three. Otherwise, return false.
+
+An integer n is a power of three, if there exists an integer x such that n == 3x.
+
+ class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        if(n == 1) {
+            return true;
+        }
+        if(n==0 || n%3!=0) {
+            return false;
+        }
+        return isPowerOfThree(n/3);
+    }
+};
